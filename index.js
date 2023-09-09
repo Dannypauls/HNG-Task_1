86823c4916cdd45b9a77af15717a7a4858169068
Dannypauls <dannypaul@localhost.localdomain>
2023-09-09 08:00:35 +0100

    Date and Time fixed

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
-const h1Tag = document.getElementsByTagName("h1")
-h1Tag.style.color('blue')
+const h1Tag = document.querySelector("h1")
+const getDay = document.getElementById('day')
+const getTime = document.getElementById('time')
+
+const date = new Date()
+
+const d = date.getDay()
+const daysOfTheWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+const day = daysOfTheWeek[d]
+getDay.innerHTML=`<strong>Day : </strong> ${day}`
+
+const t = date.getTime()
+getTime.innerHTML=`<strong>Time : </strong> ${t}`
